ad5a20d34e27ea1c7719f7dced5718a886661b37
jdy8739 <jdy8739@naver.com>
2023-01-16 23:48:49 +0900

    Chore: 마지막 프로젝트 초기 세팅

---
+++ src/containers/CommentListContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentList from "../components/CommentList";
+
+function CommentListContainer() {
+  return (
+    <div>
+      <CommentList />
+    </div>
+  );
+}
+
+export default CommentListContainer;
