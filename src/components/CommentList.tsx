ce54b9666c5f6b2da2533660b687b25eb8dc2a66
ddaisylee <elliejay9988@gmail.com>
2023-01-18 00:58:39 +0900

    Feat: CommentList 컴포넌트에 댓글 불러오기 기능 적용

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { useAppDispatch, useAppSelector } from "../redux/store";
+import { fetchComments } from "../redux/actions";
 
 const Comment = styled.div`
   padding: 7px 10px;
@@ -35,21 +37,16 @@ const Button = styled.div`
   }
 `;
 
-// 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
-const data = [
-  {
-    id: 1,
-    profile_url: "https://picsum.photos/id/1/50/50",
-    author: "abc_1",
-    content: "UI 테스트는 어떻게 진행하나요",
-    createdAt: "2020-05-01",
-  },
-];
-
 function CommentList() {
+  const comments = useAppSelector((state) => state.comments);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchComments());
+  }, [dispatch]);
+
   return (
     <>
-      {data.map((comment) => (
+      {comments.map((comment) => (
         <Comment key={comment.id}>
           <img src={comment.profile_url} alt="" />
 
